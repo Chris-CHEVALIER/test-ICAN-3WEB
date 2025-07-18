ae96099134bbe52fccf83489b5128dbc09c41bc6
Chris <chevalier@chris-freelance.com>
2025-07-18 10:51:32 +0200

    demo fetch API

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+const pokemonSection = document.getElementById('pokemon-section')
+console.log(pokemonSection)
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=800&offset=0').then(response =>
+  response.json().then(data => {
+    let pokemons = data.results
+    pokemons.forEach(pokemon => {
+      fetch(pokemon.url)
+        .then(res => res.json())
+        .then(pokemonData => {
+          let img = document.createElement('img')
+          let li = document.createElement('li')
+
+          img.src = pokemonData.sprites.other.dream_world.front_default
+          li.textContent = pokemon.name
+
+          pokemonSection.appendChild(li)
+          pokemonSection.appendChild(img)
+        })
+    })
+  })
+)
+
+console.log('Hello World')
